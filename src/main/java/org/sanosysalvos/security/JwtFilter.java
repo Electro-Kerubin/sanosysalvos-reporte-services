660234e9769592d1f81095dccb3032acdca976aa
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-06-22 02:50:34 -0400

    fix: agregando log

--- src/main/java/org/sanosysalvos/security/JwtFilter.java
+++ src/main/java/org/sanosysalvos/security/JwtFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
